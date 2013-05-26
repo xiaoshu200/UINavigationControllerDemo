9e310c0a855f804fc6603e8aca1df2e2b3316a4e
linshengqin <sqin.lin@foxmail.com>
2013-05-26 10:51:48 +0800

    Initial Commit

---
+++ UINavigationControllerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UINavigationControllerDemo
+//
+//  Created by linshengqin on 13-5-26.
+//  Copyright (c) 2013年 linshengqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
